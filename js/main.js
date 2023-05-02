a8aa35f6bf540d4aa45c0e26a357f5674291347c
aldsanms <aldryck.sanchez@viacesi.fr>
2023-05-02 10:27:53 +0200

    fix resize call

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@
 function resizeIframe() {
     var iframe = document.getElementById("iframePage");
     iframe.height = (iframe.contentWindow.document.body.scrollHeight+100) + 'px';
-    //window.scrollTo(0,0);
 }
 
-
-
-
+function loadIframe() {
+    resizeIframe();
+    window.scrollTo(0,0);
+}
